47515a272d8bb83f515a48b4bd3ae16bf7bb05e7
Ralph Irving <ralph_irving@hotmail.com>
2012-11-26 14:12:23 +0000

    Increased squeezelite revision to 177.

--- squeezelite.h
+++ squeezelite.h
@@ -35,7 +35,7 @@
 #include <sys/types.h>
 #include <poll.h>
 
-#define VERSION "v0.4beta2-175"
+#define VERSION "v0.4beta2-177"
 
 #define STREAMBUF_SIZE (2 * 1024 * 1024)
 #define OUTPUTBUF_SIZE (44100 * 8 * 10)
