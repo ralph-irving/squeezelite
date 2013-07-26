8acef2353351701eecbccffc9459ac073aaa8eac
Ralph Irving <ralph_irving@hotmail.com>
2013-07-26 12:35:05 +0000

    Added Solaris 10 resample makefile. Added Sun Ray solaris startup script. Recompiled decoder libraries with gcc 4.3.3 on Solaris 10. Increased squeezelite revision to 257.

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE or RESAMPLE to influence build
 
-#define VERSION "v1.3-dev-256"
+#define VERSION "v1.3-dev-257"
 
 // build detection
 #if defined(linux)
---
+++ squeezelite.sh
@@ -0,0 +1,36 @@
+#!/bin/ksh
+
+INSTALL_DIR=/opt/squeezelite
+export LD_LIBRARY_PATH=$INSTALL_DIR/lib
+
+if [ ! -z $SUN_SUNRAY_TOKEN ]; then
+
+	AUDIODEV=$(/opt/SUNWut/bin/utaudio $$ squeezelite)
+	trap 'pkill -f "utaudio.*$$"; exit 1' EXIT
+
+	UTPID=$(pgrep -f "utaudio.*$$")
+
+	/bin/priocntl -s -c RT -i pid $UTPID
+fi
+
+DEVICES=`$INSTALL_DIR/bin/squeezelite -l`
+
+if [ -z $DEVICES ]; then
+	echo "No audio device found."
+	exit 1
+fi
+
+if [ ! -z $UTMAC ]; then
+	MAC="-m $UTMAC"
+else
+	MAC=""
+fi
+
+AUDIOCTL=/usr/sfw/bin/audioctl
+
+if [ -x ${AUDIOCTL} ]; then
+	${AUDIOCTL} =headphone
+	${AUDIOCTL} =46%
+fi
+
+$INSTALL_DIR/bin/squeezelite $MAC $*
