dca2a5af71c821de7c147410205b20ee1473da2f
Ralph Irving <ralph_irving@hotmail.com>
2013-07-28 13:06:40 +0000

    Add resample support to solaris 9 makefile. Increased squeezelite revision to 261.

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE or RESAMPLE to influence build
 
-#define VERSION "v1.3-dev-260"
+#define VERSION "v1.3-dev-261"
 
 // build detection
 #if defined(linux)
