4810ee6bf983eaaf8ed09d51b636d46e8c7708f7
Ralph Irving <ralph_irving@hotmail.com>
2014-03-19 15:28:38 +0000

    Removed unused source files from solaris makefile. Increase squeezelite revision to 399.

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, VISEXPORT, DSD, LINKALL to influence build
 
-#define VERSION "v1.6dev-398"
+#define VERSION "v1.6dev-399"
 
 // build detection
 #if defined(linux)
