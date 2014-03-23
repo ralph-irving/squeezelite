6fea8530dd6d6ff12394ee2658112376db9a4d72
Ralph Irving <ralph_irving@hotmail.com>
2014-03-23 20:33:48 +0000

    Bump squeezelite version to 1.6 (1ca20695176cb1d970d01ca47d5d27d85d540857) Increase squeezelite revison to 401.

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, VISEXPORT, DSD, LINKALL to influence build
 
-#define VERSION "v1.6dev-400"
+#define VERSION "v1.6-401"
 
 // build detection
 #if defined(linux)
