8c13ceaaa08a3127cd48b2b9a3df582d4337b1c1
Ralph Irving <ralph_irving@hotmail.com>
2014-05-05 13:35:34 +0000

    Update solaris squeezelite run script. Increase squeezelite revision to 416.

--- scripts/squeezelite.sh
+++ scripts/squeezelite.sh
@@ -1,31 +1,27 @@
 #!/bin/ksh
 
 INSTALL_DIR=/opt/squeezelite
-export LD_LIBRARY_PATH=$INSTALL_DIR/lib
+UTSET=/opt/SUNWut/bin/utset
 
 if [ ! -z $SUN_SUNRAY_TOKEN ]; then
 
-	AUDIODEV=$(/opt/SUNWut/bin/utaudio $$ squeezelite)
-	trap 'pkill -f "utaudio.*$$"; exit 1' EXIT
+        AUDIODEV=$(/opt/SUNWut/bin/utaudio $$ squeezelite)
+        trap 'pkill -f "utaudio.*$$"; exit 1' EXIT
 
 	UTPID=$(pgrep -f "utaudio.*$$")
+	if [ ! -z $UTPID ]; then
+		/bin/priocntl -s -c RT -i pid $UTPID
+	fi
 
-	/bin/priocntl -s -c RT -i pid $UTPID
+	if [ -x ${UTSET} ]; then
+                ${UTSET} -o s=h,v=15
+        fi
+
+	if [ ! -z $AUDIODEV ]; then
+        	DEV="-o $AUDIODEV"
+	else
+        	DEV=""
+	fi
 fi
 
-DEVICES=`$INSTALL_DIR/bin/squeezelite -l`
-
-if [ -z $DEVICES ]; then
-	echo "No audio device found."
-	exit 1
-fi
-
-AUDIOCTL=/usr/sfw/bin/audioctl
-
-if [ -x ${AUDIOCTL} ]; then
-	${AUDIOCTL} =headphone
-	${AUDIOCTL} =46%
-	${AUDIOCTL} -speaker
-fi
-
-$INSTALL_DIR/bin/squeezelite $*
+$INSTALL_DIR/bin/squeezelite $DEV $*
--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, VISEXPORT, DSD, LINKALL to influence build
 
-#define VERSION "v1.6.2-414"
+#define VERSION "v1.6.2-416"
 
 // build detection
 #if defined(linux)
