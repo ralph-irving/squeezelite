1648e07fdd449b12b593c2814b16379b2d655c49
Ralph Irving <ralph_irving@hotmail.com>
2014-05-18 11:49:11 +0000

    Add debian linux oss all codecs statically linked script. Increase squeezelite revision to 418.

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, VISEXPORT, DSD, LINKALL to influence build
 
-#define VERSION "v1.6.2-416"
+#define VERSION "v1.6.2-418"
 
 // build detection
 #if defined(linux)
