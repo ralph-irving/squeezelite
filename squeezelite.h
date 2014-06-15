f5d177c7406b5367142f18842828c2b07edd23d1
Ralph Irving <ralph_irving@hotmail.com>
2014-06-15 10:40:43 +0000

    Fix RESAMPLE_MP build option (deb5f6b0b3716ef845d7dffb69e29d56b57a2367) Increase squeezelite revision to 427.

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, VISEXPORT, DSD, LINKALL to influence build
 
-#define VERSION "v1.6.3-426"
+#define VERSION "v1.6.3-427"
 
 // build detection
 #if defined(linux)
