086102bc0e300880cff8b60fc9e3c86a20e15322
Ralph Irving <ralph_irving@hotmail.com>
2014-07-06 13:37:29 +0000

    Bump version (8703a1779c2b4be32513c50fd0792274bbdc5724) Increase squeezelite revision to 435.

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, VISEXPORT, DSD, LINKALL to influence build
 
-#define VERSION "v1.6.3-434"
+#define VERSION "v1.6.4-435"
 
 // build detection
 #if defined(linux)
