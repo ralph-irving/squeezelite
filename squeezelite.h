1a50440544c27539f4e9ab9e296f02a0401b4492
Ralph Irving <ralph_irving@hotmail.com>
2014-07-08 13:44:57 +0000

    Update changelog (75a72d5f86e1675b5c5892ee5fb2278bbe8dc695) Increase squeezelite revision to 437.

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, VISEXPORT, DSD, LINKALL to influence build
 
-#define VERSION "v1.6.4-436"
+#define VERSION "v1.6.4-437"
 
 // build detection
 #if defined(linux)
