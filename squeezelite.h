2810fee152d0a4b37df04014a5dec1aa8d783ada
Ralph Irving <ralph_irving@hotmail.com>
2015-03-20 12:06:51 +0000

    Added IR support to squeezelite arm builds. Increased squeezelite revision to 584.

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, RESAMPLE_MP, VISEXPORT, IR, DSD, LINKALL to influence build
 
-#define VERSION "v1.8-583"
+#define VERSION "v1.8-584"
 
 #if !defined(MODEL_NAME)
 #define MODEL_NAME SqueezeLite
