9547428fc117c7cfb38633896cd1bc9c815f8a3c
Ralph Irving <ralph_irving@hotmail.com>
2015-07-25 14:24:10 +0000

    Added IR support to Linux OSS build. Increased squeezelite revision to 595.

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, RESAMPLE_MP, VISEXPORT, IR, DSD, LINKALL to influence build
 
-#define VERSION "v1.8-594"
+#define VERSION "v1.8-595"
 
 #if !defined(MODEL_NAME)
 #define MODEL_NAME SqueezeLite
