69fe7adbc0520460fa3b4ea2813eb15000b44e9d
Ralph Irving <ralph_irving@hotmail.com>
2015-11-24 13:43:09 +0000

    Update squeezelite armhf static build script. Increase squeezelite revision to 658.

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, RESAMPLE_MP, VISEXPORT, IR, DSD, LINKALL to influence build
 
-#define VERSION "v1.8-595"
+#define VERSION "v1.8-658"
 
 #if !defined(MODEL_NAME)
 #define MODEL_NAME SqueezeLite
