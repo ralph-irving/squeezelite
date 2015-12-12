42be902d97fc1c6c6f04836a1be1ad16234bf069
Ralph Irving <ralph_irving@hotmail.com>
2015-12-12 23:22:55 +0000

    Squelch XCode 7 compiler warning: output.c:154:22: warning: comparison of constant 'FADE_ACTIVE' (2) with boolean expression is always false                                 if (!output.fade == FADE_ACTIVE || !output.fade_mode == FADE_CROSSFADE) {                                     ~~~~~~~~~~~~ ^  ~~~~~~~~~~~ Increase squeezelite revision to 676.

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, RESAMPLE_MP, VISEXPORT, IR, DSD, LINKALL to influence build
 
-#define VERSION "v1.8-668"
+#define VERSION "v1.8-676"
 
 #if !defined(MODEL_NAME)
 #define MODEL_NAME SqueezeLite
