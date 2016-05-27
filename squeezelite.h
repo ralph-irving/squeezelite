e37ed17fed9e11a7346cbe9f1e1deeccc051f42e
Ralph Irving <ralph_irving@hotmail.com>
2016-05-27 11:42:48 +0000

    Update version in solaris build scripts. Increase squeezelite revision to 743.

--- squeezelite.h
+++ squeezelite.h
@@ -24,7 +24,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, RESAMPLE_MP, VISEXPORT, GPIO, IR, DSD, LINKALL to influence build
 
-#define VERSION "v1.8.4-737"
+#define VERSION "v1.8.4-743"
 
 #if !defined(MODEL_NAME)
 #define MODEL_NAME SqueezeLite
