4ba681ad86b4275b6d8d9e853c6f4765fa1b0904
Ralph Irving <ralph_irving@hotmail.com>
2016-08-27 18:21:53 +0000

    Merge pull request #9; Allow proper handover to Squeezenetwork Handover to Squeezenetwork fails. Address 0.0.0.1 is not properly identified as reference to mysqueezebox.com Increase squeezelite version 1.8.5, revision to 797.

--- squeezelite.h
+++ squeezelite.h
@@ -24,7 +24,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, RESAMPLE_MP, VISEXPORT, GPIO, IR, DSD, LINKALL to influence build
 
-#define VERSION "v1.8.4-785"
+#define VERSION "v1.8.5-797"
 
 #if !defined(MODEL_NAME)
 #define MODEL_NAME SqueezeLite
