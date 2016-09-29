6072f0551a13d3d53a82f1eb92a6666e49a8967f
Ralph Irving <ralph_irving@hotmail.com>
2016-09-29 14:59:46 +0000

    ffmpeg 3.1+ requires macOS 10.7+ Increase squeezelite to revision 806.

--- squeezelite.h
+++ squeezelite.h
@@ -24,7 +24,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, RESAMPLE_MP, VISEXPORT, GPIO, IR, DSD, LINKALL to influence build
 
-#define VERSION "v1.8.5-804"
+#define VERSION "v1.8.5-806"
 
 #if !defined(MODEL_NAME)
 #define MODEL_NAME SqueezeLite
