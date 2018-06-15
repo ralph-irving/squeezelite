c56dbb0293e7a35470a53f2ec8c6dff0283a05a1
Ralph Irving <ralph_irving@hotmail.com>
2018-06-15 11:23:23 +0000

    Apply change from Jeff07971 to remove GPIO and Power script hardcoded shutdown after 5 minutes regardless of the player power state. Increase squeezelite revision to 1104.

--- squeezelite.h
+++ squeezelite.h
@@ -24,7 +24,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, RESAMPLE_MP, VISEXPORT, GPIO, IR, DSD, LINKALL to influence build
 
-#define VERSION "v1.9.0-1103"
+#define VERSION "v1.9.0-1104"
 
 #if !defined(MODEL_NAME)
 #define MODEL_NAME SqueezeLite
