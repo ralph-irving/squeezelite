44c7b99ba22c96660d5d1755a169c734ba41539f
Ralph Irving <ralph_irving@hotmail.com>
2019-08-05 12:40:18 +0000

    Merge pull request #73: opus / vorbis (cleaned) In vorbis (maybe opus) it might happen that there are still some frames in the buffer but the decoder does not decode anything anyhow and stops progressing. So, when no frames is decoded and streaming has ended, still declare end of decode (opus). Thank you philippe44. Increase squeezelite revision to 1176.

--- opus.c
+++ opus.c
@@ -118,7 +118,6 @@ static decode_state opus_decompress(void) {
 
 	if (stream.state <= DISCONNECT && !_buf_used(streambuf)) {
 		UNLOCK_S;
-		LOG_INFO("end of stream");
 		return DECODE_COMPLETE;
 	}
 
@@ -220,7 +219,14 @@ static decode_state opus_decompress(void) {
 
 	} else if (n == 0) {
 
-		LOG_INFO("no frame decoded");
+		if (stream.state <= DISCONNECT) {
+			LOG_INFO("partial decode");
+			UNLOCK_O_direct;
+			UNLOCK_S;
+			return DECODE_COMPLETE;
+		} else {
+			LOG_INFO("no frame decoded");
+        }
 
 	} else if (n == OP_HOLE) {
 
