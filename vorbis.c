7c7a46ff926cd2ca4fa51ad153c9f440f9911351
philippe44 <philippe_44@outlook.com>
2019-10-17 23:39:48 -0700

    vorbis

--- vorbis.c
+++ vorbis.c
@@ -269,7 +269,6 @@ static decode_state vorbis_decode(void) {
 		if (stream.state <= DISCONNECT) {
 			LOG_INFO("partial decode");
 			UNLOCK_O_direct;
-			UNLOCK_S;
 			return DECODE_COMPLETE;
 		} else {
 			LOG_INFO("no frame decoded");
