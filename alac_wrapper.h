834103360ff46214696e34451b4a31bf0435efea
Philippe G <philippe44@users.noreply.github.com>
2021-01-11 21:08:25 -0800

    fix alac click & improve memory allocation

--- alac_wrapper.h
+++ alac_wrapper.h
@@ -28,7 +28,7 @@ extern "C" {
 
 struct alac_codec_s *alac_create_decoder(int magic_cookie_size, unsigned char *magic_cookie,
 								unsigned char *sample_size, unsigned *sample_rate,
-								unsigned char *channels);
+								unsigned char *channels, unsigned int *block_size);
 void alac_delete_decoder(struct alac_codec_s *codec);
 bool alac_to_pcm(struct alac_codec_s *codec, unsigned char* input,
 				 unsigned char *output, char channels, unsigned *out_frames);
