9aae502bed278a30e96f561f8ee0d9ac1aef3496
Ralph Irving <ralph_irving@hotmail.com>
2024-01-02 16:51:16 +0000

    Update copyright notices for 2024. Increase squeezelite revision to 1462.

--- flac.c
+++ flac.c
@@ -2,6 +2,7 @@
  *  Squeezelite - lightweight headless squeezeplay emulator for linux
  *
  *  (c) Adrian Smith 2012, triode1@btinternet.com
+ *      Ralph Irving 2015-2024, ralph_irving@hotmail.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
