419e3507f5cd136510da9b04eb23c1a962d37465
Ralph Irving <ralph_irving@hotmail.com>
2024-04-14 15:42:07 +0000

    Fix for issue #221: mp3 playback on Alpine Linux (musl-libc). The check for OggFlac chaining support in libflac does not call dlerror(). dlerror() will report whatever error occurred since it's last call. Since the next call to dlerror happens when libmad (mp3) is loading, that's when the error shows up. May not be a musl-libc specific problem. Thank you for the patch paul-1. Increase squeezelite revision to 1486.

--- flac.c
+++ flac.c
@@ -346,9 +346,12 @@ static bool load_flac() {
 	// ignore error for this new API
 	f->FLAC__stream_decoder_set_ogg_chaining = dlsym(handle, "FLAC__stream_decoder_set_ogg_chaining");
 	if (!f->FLAC__stream_decoder_set_ogg_chaining) {
+               if ((err = dlerror()) != NULL) {
+                       LOG_INFO("dlerror: %s", err);
+               }
 		LOG_INFO("OggFlac chaining disabled");
 	}
-	
+
 	LOG_INFO("loaded %s", name);
 #elif !defined(FLAC__OGG_CHAINING)
 	LOG_INFO("OggFlac chaining disabled");
