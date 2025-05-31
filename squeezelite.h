0c5fac20dec6aedc48ac18809b6b954f53e710e8
Vincent Legoll <vincent.legoll@gmail.com>
2025-05-31 14:41:15 +0200

    squeezelite header file: handle OpenBSD case
    FROM: Stuart Henderson (sthen@)
    
    Code taken from here:
    https://github.com/openbsd/ports/commit/ae2290d52fed9dae56b81dd1dce933cbea57f504
    
    Fixes: https://github.com/ralph-irving/squeezelite/issues/242
    
    Signed-off-by: Vincent Legoll <vincent.legoll@gmail.com>

--- squeezelite.h
+++ squeezelite.h
@@ -61,7 +61,7 @@
 #define WIN       1
 #define PORTAUDIO 1
 #define FREEBSD   0
-#elif defined(__FreeBSD__)
+#elif defined(__FreeBSD__) || defined(__OpenBSD__)
 #define LINUX     0
 #define OSX       0
 #define WIN       0
